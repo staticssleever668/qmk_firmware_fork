490669ca94def988be433d0fff270b7247a79870
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2026-05-27 02:41:45 +0300

    feat: add vial for dk64rgb

---
+++ keyboards/dk64rgb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// Match the original DK64RGB
+#define VIAL_KEYBOARD_UID {0x83, 0x25, 0x36, 0xE0, 0x49, 0x08, 0x11, 0x7A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
